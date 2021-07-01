ba05e4a0f6a441df048126d44364bb92f2494722
babie <babie.wildheart@gmail.com>
2021-07-02 03:05:25 +0900

    chore: add eslint-config-prettier and setup it

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -1,28 +1,24 @@
 import { useRouter } from 'next/router'
 
 type Route = {
-  action: "tree" | "show" |  "new" | "edit"
+  action: 'tree' | 'show' | 'new' | 'edit'
   path: string[]
 }
 
 const getRoute = (path: string | string[] | undefined): Route => {
-  if (path && typeof path !== "string") {
+  if (path && typeof path !== 'string') {
     switch (path[0]) {
-      case "tree:": {
-        return {action: "tree", path: path.slice(1)}
-      }
-      case "new:": {
-        return {action: "new", path: path.slice(1)}
-      }
-      case "edit:": {
-        return {action: "edit", path: path.slice(1)}
-      }
-      default: {
-        return {action: "show", path: path}
-      }
+      case 'tree:':
+        return { action: 'tree', path: path.slice(1) }
+      case 'new:':
+        return { action: 'new', path: path.slice(1) }
+      case 'edit:':
+        return { action: 'edit', path: path.slice(1) }
+      default:
+        return { action: 'show', path: path }
     }
   }
-  return {action: "show", path: []}
+  return { action: 'show', path: [] }
 }
 
 const RouteSwitch = () => {
@@ -31,11 +27,11 @@ const RouteSwitch = () => {
 
   const route = getRoute(path)
   switch (route.action) {
-    case "tree":
+    case 'tree':
       return <p>tree</p>
-    case "new":
+    case 'new':
       return <p>new</p>
-    case "edit":
+    case 'edit':
       return <p>edit</p>
   }
   return <p>show</p>
