01e85f36a9c558c48c3c3c9c45d4b5c66abe7aae
babie <babie.wildheart@gmail.com>
2021-07-03 15:52:52 +0900

    feat: show `*.md` files

--- pages/[[...path]].tsx
+++ pages/[[...path]].tsx
@@ -1,37 +1,74 @@
-import type { GetStaticPaths, GetStaticProps, GetServerSideProps } from 'next'
+import type { GetStaticPaths, GetStaticProps } from 'next'
+import Head from 'next/head'
+import fs from 'fs'
 import npath from 'path'
+import fg from 'fast-glob'
 
-/*
+const getContent = async (fullpath: string): Promise<Props> => {
+  const md = fs.readFileSync(fullpath, { encoding: 'utf-8' })
+  // TODO: markdown to html
+  const title = 'Pseudo Title'
+  const html = md
+  return { title, html }
+}
+
 export const getStaticPaths: GetStaticPaths = async () => {
-  // TODO: traverse `/public` directory
-  const publicDir = npath.join(process.cwd(), 'public')
+  const paths = fg
+    .sync('**/*.md', {
+      cwd: npath.join(process.cwd(), 'public'),
+      onlyFiles: true,
+    })
+    .map((file) => ({
+      params: {
+        path: file.replace(/\.md$/, '').split('/'),
+      },
+    }))
+
   return {
-    paths: [{ params: { path: ['path', 'to', 'file'] } }],
+    paths,
     fallback: false,
   }
 }
 
-export const getStaticProps: GetStaticProps = async ({ params }) => {
-  // TODO: read markdown file
-  return {
-    props: {
-      path: [],
-    },
+type Props = {
+  title: string
+  html: string
+}
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  if (params && params.path && typeof params.path !== 'string') {
+    const fullpath = npath.join(
+      process.cwd(),
+      'public',
+      `${params.path.join('/')}.md`
+    )
+    const { title, html } = await getContent(fullpath)
+    return {
+      props: {
+        title,
+        html,
+      },
+    }
   }
+  return { notFound: true }
 }
 
-export const getServerSideProps: GetServerSideProps = async ({ params }) => {
-  // TODO: read markdown file
-  return {
-    props: {
-      path: [],
-    },
-  }
-}
-*/
-
-const Show = () => {
-  return <div>Show Show</div>
+const Show: React.FC<Props> = ({ title, html }) => {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+      </Head>
+      <main>
+        <article>
+          <h1>{title}</h1>
+          <div>{html}</div>
+          {/*
+            <div dangerouslySetInnerHTML={{ __html: html }} />
+          */}
+        </article>
+      </main>
+    </>
+  )
 }
 
 export default Show
