a5e396098e2fa8e8949650b122f3a08aa37abdf4
babie <babie.wildheart@gmail.com>
2021-07-19 21:26:28 +0900

    feat: show pathname on `tree` page

--- pages/__self__/tree/[[...path]].tsx
+++ pages/__self__/tree/[[...path]].tsx
@@ -1,5 +1,16 @@
-const Tree: React.FC = () => {
-  return <div>Tree Tree</div>
+import type { NextRouter } from 'next/router'
+import { withRouter } from 'next/router'
+
+type Props = {
+  router: NextRouter
+}
+const Tree: React.FC<Props> = ({ router }) => {
+  const path = router.query && router.query.path
+  let pathname = '/'
+  if (path && typeof path !== 'string') {
+    pathname = path.reduce<string[]>((acc, p) => [...acc, `/${p}`], []).join('')
+  }
+  return <div>Tree {pathname}</div>
 }
 
-export default Tree
+export default withRouter(Tree)
